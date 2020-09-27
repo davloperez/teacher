7169f28f4d35d677774f348403b879555e9079fe
David López Pérez <david@connectif.es>
2020-09-27 16:21:10 +0200

    Added heroku configuration

--- src/server/main.ts
+++ src/server/main.ts
@@ -15,7 +15,7 @@ server.get('/', function (req, reply) {
     return reply.sendFile('index.html', join(__dirname, clientLocalPath));
 } as any);
 
-server.listen(3000, (error, address) => {
+server.listen(+process.env.PORT, (error, address) => {
     if (error) {
         server.log.error(error);
         process.exit(1);
